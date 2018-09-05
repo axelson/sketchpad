768f6007c018889eaed06824ebefccb5aadc12a4
Jason Axelson <jason@hobnob.io>
2018-09-05 09:19:17 -0700

    Initial set of changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,3 @@ import css from "../css/app.css"
 import "phoenix_html"
 import {Socket, Presence} from "phoenix"
 import {Sketchpad, sanitize} from "./sketchpad"
-
-
-alert("You're all set!")
--- lib/sketchpad_web/controllers/page_controller.ex
+++ lib/sketchpad_web/controllers/page_controller.ex
@@ -1,7 +1,32 @@
 defmodule SketchpadWeb.PageController do
   use SketchpadWeb, :controller
 
+  @user_salt "user token"
+
+
+  plug :require_user when action not in [:signin]
+
   def index(conn, _params) do
     render(conn, "index.html")
+  end
+
+  def signin(conn, %{"user" => %{"username" => user}}) do
+    # We would check that the user exists
+    conn
+    |> put_session(:user_id, user)
+    |> redirect(to: "/")
+  end
+
+  def require_user(conn, _) do
+    if user = get_session(conn, :user_id) do
+      conn
+      |> assign(:user_id, user)
+      |> assign(:user_token, Phoenix.Token.sign(conn, @user_salt, user))
+    else
+      conn
+      |> put_flash(:error, "Please sign-in!")
+      |> render("signin.html")
+      |> halt()
+    end
   end
 end
